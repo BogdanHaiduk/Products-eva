7ad427592adebd396ad9d4c7eb4d4e41ccc6ed50
Bohdan <haidukbogdan@gmail.com>
2022-11-02 14:50:21 +0200

    Created an entity and her repository

---
+++ src/main/java/com/products/eva/db/entity/Product.java
@@ -0,0 +1,40 @@
+package com.products.eva.db.entity;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Product {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "description")
+    String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+}
---
+++ src/main/java/com/products/eva/db/repository/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.products.eva.db.repository;
+
+import com.products.eva.db.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Long> {
+}
